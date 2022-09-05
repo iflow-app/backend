f519419d74809f471857e2c11930a90bb5174d9e
Rogério Caixeta <junior.rogerio8@gmail.com>
2022-09-05 19:48:24 -0300

    feat(user): :sparkles: Add user authentication use case

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: IUserRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "5824f6066d9f703120eceefcb2c0a026", {
+      subject: user.user_id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+
+import { authenticateRoutes } from "./authenticate.routes";
 import { userRoutes } from "./user.routes";
 
 const router = Router();
 
 router.use("/user", userRoutes);
+router.use(authenticateRoutes);
 
 export { router };
