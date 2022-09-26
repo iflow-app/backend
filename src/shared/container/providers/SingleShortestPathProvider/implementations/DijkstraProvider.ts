bcd7deaf3e0312447b38558914b4a41471f6164b
Rogério Caixeta <junior.rogerio8@gmail.com>
2022-09-26 20:35:07 -0300

    fix(provider/ssp): :bug: Adjust node conections before adding it to the graph

--- src/shared/container/providers/SingleShortestPathProvider/implementations/DijkstraProvider.ts
+++ src/shared/container/providers/SingleShortestPathProvider/implementations/DijkstraProvider.ts
@@ -12,17 +12,27 @@ class DijkstraProvider implements ISingleShortestPathProvider {
   run(data: HouseOfQuality[]): number[] {
     const graph = new Graph();
 
-    const nfunctionalIds = data.map((relation) => {
-      const { nfunctional_id, functional_id, weight } = relation;
-      graph.addNode(nfunctional_id.toString(), {
-        [functional_id.toString()]: weight,
-      });
-      graph.addNode(functional_id.toString(), {
-        [nfunctional_id.toString()]: weight,
-      });
+    const relations = {};
+    const nfunctionalIds = [];
 
-      return relation.nfunctional_id;
+    data.map((relation) => {
+      const { weight } = relation;
+
+      const functional_id = relation.functional_id.toString();
+      const nfunctional_id = relation.nfunctional_id.toString();
+
+      if (!(functional_id in relations)) relations[functional_id] = {};
+      if (!(nfunctional_id in relations)) relations[nfunctional_id] = {};
+
+      relations[functional_id][nfunctional_id] = weight;
+      relations[nfunctional_id][functional_id] = weight;
+
+      nfunctionalIds.push(nfunctional_id);
+
+      return null;
     });
+
+    Object.keys(relations).map((key) => graph.addNode(key, relations[key]));
 
     const uniqueNfunctionalIds = [...new Set(nfunctionalIds)];
 
